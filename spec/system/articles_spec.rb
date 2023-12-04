b88dcb4f4e3ce55be41a4e259895821f0ee865f5
Ross Kaffenberger <rosskaff@gmail.com>
2023-12-03 21:17:22 -0500

    Build basic layouts for header and footer

--- spec/system/articles_spec.rb
+++ spec/system/articles_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
-RSpec.describe "Blog", type: :system do
+RSpec.describe "Articles", type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  it "displays blog" do
-    visit "/blog"
+  it "displays articles" do
+    visit "/articles"
 
     expect(page).to have_content("Hello world")
   end
