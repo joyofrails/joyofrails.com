4127b0e9b6350cc6853cc661750828b5b82561bf
Ross Kaffenberger <rosskaff@gmail.com>
2023-12-16 10:53:56 -0500

    Update colors

--- config/tailwind/colors.js
+++ config/tailwind/colors.js
@@ -1,23 +1,14 @@
 module.exports = {
   // Gray definitions
-  'gray-1': 'var(--slate-1)',
   'gray-base': 'var(--slate-1)',
   'gray-bg-subtle': 'var(--slate-2)',
   'gray-bg': 'var(--slate-3)',
   'gray-bg-hover': 'var(--slate-4)',
   'gray-bg-active': 'var(--slate-5)',
-  'gray-line': 'var(--slate-6)',
-  'gray-border': 'var(--slate-7)',
-  'gray-border-hover': 'var(--slate-8)',
-  'gray-solid': 'var(--slate-9)',
-  'gray-solid-hover': 'var(--slate-10)',
-  'gray-solid-text': '#fff',
   'gray-text': 'var(--slate-11)',
-  'gray-12': 'var(--slate-12)',
   'gray-text-contrast': 'var(--slate-12)',
   'gray-solid-high-contrast': 'var(--slate-12)',
   'gray-solid-high-contrast-text': 'var(--gray-1)',
-  'gray-2-translucent': '#1b1d1eb3',
 
   // Accent definitions
   'theme-title': 'var(--theme-title)',
