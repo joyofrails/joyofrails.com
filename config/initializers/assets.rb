240b6c5095c791ff83479e5005afa292cc3131c9
Ross Kaffenberger <rosskaff@gmail.com>
2023-12-26 17:04:57 -0500

    Attempt to fix svg compilation in production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("app", "content", "pages", "articles")
+Rails.application.config.assets.paths << %w(.svg)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
