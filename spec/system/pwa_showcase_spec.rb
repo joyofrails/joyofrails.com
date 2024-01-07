123718e42396e1a456b8403f4bfb6f4f45685526
Ross Kaffenberger <rosskaff@gmail.com>
2024-01-06 20:18:08 -0500

    Add link to PWA showcase

--- spec/system/pwa_showcase_spec.rb
+++ spec/system/pwa_showcase_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 RSpec.describe "PWA Showcase", type: :system do
   it "renders the Install to Homescreen button" do
-    visit "/pwa-showcase"
+    visit "/"
+
+    click_link "Progress Web Apps on Rails Showcase"
 
     expect(page).to have_button("Install to Homescreen")
   end
@@ -13,8 +15,9 @@ RSpec.describe "PWA Showcase", type: :system do
     end
 
     it "renders the Install to Homescreen button" do
-      visit "/pwa-showcase"
+      visit "/"
 
+      click_link "Progress Web Apps on Rails Showcase"
       click_link "Install to Homescreen"
 
       expect(page).to have_content("PWA installation is not currently supported in Firefox.")
