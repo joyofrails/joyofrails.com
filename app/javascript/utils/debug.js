0c0da33619f1541f3f7d19fa3d0f6df8e8278167
Ross Kaffenberger <rosskaff@gmail.com>
2024-01-20 07:47:33 -0500

    Add debug.js wrapper to use console log, error, and warn

---
+++ app/javascript/utils/debug.js
@@ -0,0 +1,17 @@
+import debug from 'debug';
+
+export default function (namespace) {
+  return {
+    log: debug(namespace),
+    error: (() => {
+      const error = debug(namespace);
+      error.log = console.error.bind(console);
+      return error;
+    })(),
+    warn: (() => {
+      const warn = debug(namespace);
+      warn.log = console.warn.bind(console);
+      return warn;
+    })(),
+  };
+}
