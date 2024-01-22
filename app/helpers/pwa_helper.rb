d1c1840a84f4f53dc6e03c501de546c6b53b43c8
Ross Kaffenberger <rosskaff@gmail.com>
2024-01-22 07:32:49 -0500

    Push manager registration accepts unpadded url-safe base64 encoded key
    Source:
    https://www.w3.org/TR/push-api/#dom-pushsubscriptionoptions
    
    The applicationServerKey attribute, when getting, returns the value it
    was initialized with.
    
    If present, the value of applicationServerKey MUST include a point on
    the P-256 elliptic curve [DSS], encoded in the uncompressed form
    described in [ANSI-X9-62] Annex A (that is, 65 octets, starting with an
    0x04 octet). When provided as a DOMString, the value MUST be encoded
    using the base64url encoding [RFC7515].

--- app/helpers/pwa_helper.rb
+++ app/helpers/pwa_helper.rb
@@ -1,5 +1,5 @@
 module PwaHelper
   def web_push_key
-    Base64.encode64(Base64.urlsafe_decode64(Rails.application.credentials.vapid.public_key))
+    Rails.application.credentials.vapid.public_key.delete("=")
   end
 end
