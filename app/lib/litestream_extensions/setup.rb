bfc1a39fae239dc8760d2e84fc8277e1c9810762
Ross Kaffenberger <rosskaff@gmail.com>
2024-03-02 16:57:25 -0500

    Bump litestream gem, use original command
    The most recent version of the litestream gem uses fork to initiate the
    litestream server which should address the honeybadger signal exception
    errors we get on deployment.
    
    https://github.com/fractaledmind/litestream-ruby/pull/6
    
    To play nicely with the built-in command, we now write our custom
    configuration to `config/litestream.yml`.

--- app/lib/litestream_extensions/setup.rb
+++ app/lib/litestream_extensions/setup.rb
@@ -1,30 +1,32 @@
 module LitestreamExtensions
   module Setup
-    DATABASES_FOR_LITESTREAM = %w[data metrics queue]
-
     module_function
 
     def configure_litestream
       File.write(config_path, YAML.dump(litestream_config))
     end
 
+    def databases
+      ActiveRecord::Base
+        .configurations
+        .configs_for(env_name: Rails.env, include_hidden: true)
+        .select { |config| ["sqlite3", "litedb"].include? config.adapter }
+        .map(&:database)
+    end
+
     def config_path
-      Rails.root.join("config", "litestream", "#{Rails.env}.yml")
+      Rails.root.join("config", "litestream.yml")
     end
 
     def litestream_config
       {
-        "dbs" => DATABASES_FOR_LITESTREAM.map do |db|
+        "dbs" => databases.map do |db|
           {
-            "path" => File.join(litestack_data_path, Rails.env, "#{db}.sqlite3"),
-            "replicas" => [{"url" => "s3://#{litestream_bucket}/#{Rails.env}/#{db}.sqlite3"}]
+            "path" => db,
+            "replicas" => [{"url" => "s3://#{litestream_bucket}/#{db}"}]
           }
         end
       }
-    end
-
-    def litestack_data_path
-      ENV.fetch("LITESTACK_DATA_PATH", "./storage")
     end
 
     def litestream_bucket
