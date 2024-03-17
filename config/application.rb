b0d5d89b3c512a7c87cb05caaddbe859783b6c0b
Ross Kaffenberger <rosskaff@gmail.com>
2024-03-17 10:59:44 -0400

    Remove application.rb solid cache settings
    SolidCache 0.5+ now respects settings in config/solid_cache.yml.
    
    Also updates the config file that was previously being ignored: since
    database files are already separated by environment, there's no need to
    namespace rails cache storage keys.

--- config/application.rb
+++ config/application.rb
@@ -42,10 +42,6 @@ module Joy
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    if SolidCache::VERSION > "0.4.2" && Rails.env.local?
-      raise "Check to see if we can remove this config setting below"
-    end
-    config.solid_cache.connects_to = {database: {writing: :cache, reading: :cache}}
     config.solid_queue.connects_to = {database: {writing: :queue, reading: :queue}}
 
     # Log to STDOUT
