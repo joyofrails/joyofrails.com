1aa9583cfe56d936b18f509a5e79012da0af912d
Ross Kaffenberger <rosskaff@gmail.com>
2024-03-17 09:05:58 -0400

    Provide env vars to customize Rails logging from CLI
    Log to $stdout by default in development with Procfile
    
    Tune down logging for tests and Solid Queue in development.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Limit logging in test environment
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "warn")
 end
