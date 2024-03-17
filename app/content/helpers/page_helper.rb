da99ce311250757a60ea958e70390b422de4502e
Ross Kaffenberger <rosskaff@gmail.com>
2024-03-17 10:58:06 -0400

    Re-lint with updated standard

--- app/content/helpers/page_helper.rb
+++ app/content/helpers/page_helper.rb
@@ -33,8 +33,8 @@ module PageHelper
 
   # Render a block within a layout. This is a useful, and prefered way, to handle
   # nesting layouts, within Sitepress.
-  def render_layout(layout, **, &block)
-    render(html: capture(&block), layout: "layouts/#{layout}", **)
+  def render_layout(layout, **, &)
+    render(html: capture(&), layout: "layouts/#{layout}", **)
   end
 
   def render_toc(page)
