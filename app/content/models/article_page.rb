68251b4b0d1ec506ca1513482ed50fbfbf6cdcf7
Ross Kaffenberger <rosskaff@gmail.com>
2024-04-25 15:02:02 -0400

    Enable viewing of drafts

--- app/content/models/article_page.rb
+++ app/content/models/article_page.rb
@@ -1,6 +1,19 @@
 class ArticlePage < Sitepress::Model
   collection glob: "articles/*.html*"
-  data :title, :published_on, :summary, :description, :tags
+  data :title, :published, :summary, :description, :tags
+
+  def self.published
+    all.filter { |article| article.published.present? }.sort_by { |article| article.published_on }
+  end
+
+  def self.draft
+    all.filter { |article| article.published.blank? }
+  end
+
+  def published_on
+    published.to_date
+  end
+  alias_method :published_at, :published_on
 
   def gravatar_image_url
     "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
--- app/helpers/articles_helper.rb
+++
@@ -1,9 +0,0 @@
-module ArticlesHelper
-  def articles_list
-    articles = ArticlePage.all
-    if Rails.env.production?
-      articles = articles.select { |article| !article.data.draft? }
-    end
-    articles
-  end
-end
