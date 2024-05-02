a16d27c8e04fe9cf57aef08daf7d5753d40da421
Ross Kaffenberger <rosskaff@gmail.com>
2024-05-02 06:38:09 -0400

    Add test for article

---
+++ spec/system/articles/introducing_joy_of_rails_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Introducing Joy of Rails", type: :system do
+  it "displays the article content" do
+    visit "/articles/introducing-joy-of-rails"
+
+    expect(page).to have_content "Introducing Joy of Rails"
+
+    page.driver.scroll_to(0, -10000)
+    within "#new_examples_counter" do
+      expect(page).to have_content("Count\n0")
+
+      click_button "Increment"
+
+      expect(page).to have_content("Count\n1")
+    end
+  end
+end
