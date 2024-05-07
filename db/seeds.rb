35f2bfa06d0a47aaeac1576d130f35edc0301c04
Ross Kaffenberger <rosskaff@gmail.com>
2024-05-07 07:30:18 -0400

    Update db seeds for cold startup locally

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,3 @@ if File.exist?(Rails.root.join("db", "seeds", "#{Rails.env}.rb"))
   puts "Loading #{Rails.env} seeds..."
   require Rails.root.join("db", "seeds", "#{Rails.env}.rb")
 end
-
-# Create admin user account
-AdminUser.find_or_create_by!(
-  email: Rails.application.credentials.seeds.admin_email
-) do |u|
-  password = Rails.application.credentials.seeds.admin_password
-  u.password = password
-  u.password_confirmation = password
-end
