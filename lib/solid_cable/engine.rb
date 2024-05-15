1644c3c419e15569d9bc31db2bc09d23f5d7637c
Ross Kaffenberger <rosskaff@gmail.com>
2024-05-15 10:02:23 -0400

    Fix for solid cable config eager loading in production

--- lib/solid_cable/engine.rb
+++ lib/solid_cable/engine.rb
@@ -4,12 +4,15 @@ module SolidCable
   class Engine < ::Rails::Engine
     isolate_namespace SolidCable
 
-    config.solid_cable = ActiveSupport::OrderedOptions.new
+    # Engine configuration moved to initializer to allow for
+    # lazy loading in development
 
-    initializer "solid_cable.config" do
-      config.solid_cable.each do |name, value|
-        SolidCable.public_send(:"#{name}=", value)
-      end
-    end
+    # config.solid_cable = ActiveSupport::OrderedOptions.new
+
+    # initializer "solid_cable.config" do
+    #   config.solid_cable.each do |name, value|
+    #     SolidCable.public_send(:"#{name}=", value)
+    #   end
+    # end
   end
 end
