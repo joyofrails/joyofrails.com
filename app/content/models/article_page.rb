2e4dd4c62aecfa93a8cd5e2dd48e11a56029923d
Ross Kaffenberger <rosskaff@gmail.com>
2024-06-02 19:14:36 -0400

    Enhance ArticlePage for atom feeds

--- app/content/models/article_page.rb
+++ app/content/models/article_page.rb
@@ -1,6 +1,11 @@
 class ArticlePage < Sitepress::Model
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+
   collection glob: "articles/*.html*"
-  data :title, :published, :summary, :description, :tags
+  data :title, :published, :updated, :summary, :description, :tags
+
+  delegate :mime_type, :handler, to: :page
 
   def self.published
     all.filter { |article| article.published.present? }.sort_by { |article| article.published_on }
@@ -10,12 +15,30 @@ class ArticlePage < Sitepress::Model
     all.filter { |article| article.published.blank? }
   end
 
+  def id
+    File.basename(request_path)
+  end
+
+  def persisted?
+    false
+  end
+
   def published_on
     published.to_date
   end
   alias_method :published_at, :published_on
+  alias_method :created_at, :published_on
+
+  def updated_on
+    updated&.to_date || published_on
+  end
+  alias_method :updated_at, :updated_on
 
   def gravatar_image_url
     "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
+  end
+
+  def author
+    data.author || "Ross Kaffenberger"
   end
 end
