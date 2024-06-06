10c8a2033b7a1725cd6cb45de9dce0e8176f5849
Ross Kaffenberger <rosskaff@gmail.com>
2024-06-06 07:41:02 -0400

    Fix user migration

--- db/migrate/20240605030240_create_users.rb
+++ db/migrate/20240605030240_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_05_031226) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "confirmed_at"
