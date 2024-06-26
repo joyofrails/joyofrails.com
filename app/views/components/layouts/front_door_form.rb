2f796048f9ce5d8d7047912b085812fc84388c6b
Ross Kaffenberger <rosskaff@gmail.com>
2024-06-25 23:05:56 -0400

    Style updates for forms

--- app/views/components/layouts/front_door_form.rb
+++ app/views/components/layouts/front_door_form.rb
@@ -16,13 +16,13 @@ class Layouts::FrontDoorForm < Phlex::HTML
 
   def form_layout(&block)
     div(class: "flex min-h-full flex-col justify-center container py-xl lg:px-3xl") do
-      div(class: "mx-auto w-full max-w-sm") do
+      div(class: "mx-auto w-full max-w-sm text-theme") do
         plain inline_svg_tag "joy-logo.svg",
           class: "fill-current mx-auto",
           style: "max-width: 64px;",
           alt: "Joy of Rails"
         h2(
-          class: "mt-4 text-center text-2xl font-bold leading-9 tracking-tight"
+          class: "mt-4 text-center text-2xl text-theme font-bold leading-9 tracking-tight"
         ) { @title }
       end
       div(class: "mx-auto w-full max-w-sm", &block)
@@ -61,7 +61,7 @@ class Layouts::FrontDoorForm < Phlex::HTML
       plain form.send(
         method,
         *args,
-        class: "block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+        class: "block w-full rounded-md py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
         **opts
       )
     end
