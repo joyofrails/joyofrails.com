4c37be63f19d0c9d7371178dbecb3aa55c675719
Ross Kaffenberger <rosskaff@gmail.com>
2024-07-03 12:01:13 -0400

    Add request specs for color schemes

--- spec/system/settings/color_schemes_spec.rb
+++ spec/system/settings/color_schemes_spec.rb
@@ -3,16 +3,20 @@
 require "rails_helper"
 
 RSpec.describe "Customize Color Scheme", type: :system do
-  it "user can selected a curated color scheme" do
+  let(:curated_colors) do
     curated_color_names = YAML.load_file(Rails.root.join("config", "curated_colors.yml")).sample(3)
 
-    curated_colors = curated_color_names.map do |name|
+    curated_color_names.map do |name|
       FactoryBot.create(:color_scheme, name: name)
     end
+  end
 
+  before { curated_colors }
+
+  it "user can selected a curated color scheme" do
     visit settings_color_scheme_path
 
-    chosen_color = curated_colors.second
+    chosen_color = curated_colors.sample
     select chosen_color.display_name, from: "settings[color_scheme_id]"
 
     click_button "Save #{chosen_color.display_name}"
