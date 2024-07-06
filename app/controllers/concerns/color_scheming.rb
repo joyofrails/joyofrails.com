b0f719bd284972ebf86923396f7147e27e84616c
Ross Kaffenberger <rosskaff@gmail.com>
2024-07-06 08:09:24 -0400

    Reorder methods

--- app/controllers/concerns/color_scheming.rb
+++ app/controllers/concerns/color_scheming.rb
@@ -16,15 +16,15 @@ module ColorScheming
     preview_color_scheme_id.present? || session_color_scheme_id.present?
   end
 
-  def preview_color_scheme_id = params.dig(:settings, :color_scheme_id)
-
-  def session_color_scheme_id = session[:color_scheme_id]
-
   def find_color_scheme
     preview_color_scheme || session_color_scheme || default_color_scheme
   end
 
+  def preview_color_scheme_id = params.dig(:settings, :color_scheme_id)
+
   def preview_color_scheme = preview_color_scheme_id && ColorScheme.find(preview_color_scheme_id)
+
+  def session_color_scheme_id = session[:color_scheme_id]
 
   def session_color_scheme = session_color_scheme_id && ColorScheme.find(session_color_scheme_id)
 
