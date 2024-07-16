e369df11f971a4df3af428e08ac0b36d6c2a1e61
Ross Kaffenberger <rosskaff@gmail.com>
2024-07-15 22:55:48 -0400

    Configure VCR and Webmock
    Disable most external requests with WebMock
    
    Use VCR only with RSpec :vcr metadata

---
+++ spec/support/webmock_vcr.rb
@@ -0,0 +1,29 @@
+require "webmock/rspec"
+require "vcr"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+  config.ignore_localhost = true
+  config.configure_rspec_metadata!
+end
+
+RSpec.configure do |config|
+  config.before(:each) do
+    WebMock.disable_net_connect!(
+      allow_localhost: true,
+      allow: [
+        "chromedriver.storage.googleapis.com",
+        "validator.w3.org"
+      ]
+    )
+  end
+
+  config.around do |example|
+    if example.metadata.key?(:vcr)
+      example.run
+    else
+      VCR.turned_off { example.run }
+    end
+  end
+end
