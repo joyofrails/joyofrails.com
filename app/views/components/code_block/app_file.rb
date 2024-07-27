ac83529c64e3c6d9688a59c8c2164494d5193c95
Ross Kaffenberger <rosskaff@gmail.com>
2024-07-26 23:18:34 -0400

    Add quiet border to code block and use pygment css for app file name

--- app/views/components/code_block/app_file.rb
+++ app/views/components/code_block/app_file.rb
@@ -12,8 +12,8 @@ class CodeBlock::AppFile < Phlex::HTML
   def view_template
     render ::CodeBlock::Article.new(**attributes) do |code_block|
       code_block.title do
-        a(href: app_file.repo_url, target: "_blank", class: "flex items-center gap-1") {
-          plain app_file.filename
+        a(href: app_file.repo_url, target: "_blank", class: "nc flex items-center gap-1") {
+          plain app_file.app_path.to_s
           plain inline_svg_tag("external-link.svg", class: "icon icon-sm", height: 12, width: 12)
         }
       end
