43e0af21282a4eb79f82aed6cede9baa907609ec
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-02 22:59:21 -0400

    Update meta tags and comments in head

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def seo_meta_tags
+    set_meta_tags icon: [
+      {rel: "icon", href: "/favicon.ico?v1.0", sizes: "32x32"},
+      {rel: "icon", href: "/icon.svg?v1.0", type: "image/svg+xml"},
+      {rel: "apple-touch-icon", href: "/apple-touch-icon.png?v1.0"},
+      {rel: "manifest", href: "/manifest.webmanifest?v1.0"}
+    ]
+    display_meta_tags site: "Joy of Rails"
+  end
 end
