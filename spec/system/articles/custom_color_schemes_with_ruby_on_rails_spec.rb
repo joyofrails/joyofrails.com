f2249d44538f44aa0684ccf8d10e54ba49c8dd5c
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-03 11:13:44 -0400

    Add regression system spec for custom color scheme article

---
+++ spec/system/articles/custom_color_schemes_with_ruby_on_rails_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Custom Color Schemes with Ruby on Rails", type: :system do
+  let(:curated_colors) do
+    FactoryBot.create_list(:color_scheme, 3)
+  end
+
+  before do
+    allow(ColorScheme).to receive(:curated).and_return(curated_colors)
+  end
+
+  it "user can selected a curated color scheme" do
+    visit "/articles/custom-color-schemes-with-ruby-on-rails"
+
+    expect(page).to have_content "Custom Color Schemes with Ruby on Rails"
+
+    within "#color-scheme-form" do
+      chosen_color = curated_colors.sample
+      select chosen_color.display_name, from: "settings[color_scheme_id]"
+
+      click_button "Save #{chosen_color.display_name}"
+
+      expect(page).to have_content("You have saved #{chosen_color.display_name} as your personal color scheme.")
+
+      expect(page).to have_css(".color-scheme__#{chosen_color.name.parameterize}")
+    end
+  end
+end
