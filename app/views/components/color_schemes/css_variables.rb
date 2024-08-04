fe082cc4327ba1fe624114e6f58af10cbe2e4ddb
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-04 07:25:55 -0400

    Inline default color scheme to static css

--- app/views/components/color_schemes/css_variables.rb
+++ app/views/components/color_schemes/css_variables.rb
@@ -1,8 +1,9 @@
 class ColorSchemes::CssVariables < Phlex::HTML
   attr_reader :color_scheme
 
-  def initialize(color_scheme:)
+  def initialize(color_scheme:, my_color: false)
     @color_scheme = color_scheme
+    @my_color = my_color
   end
 
   def view_template
@@ -11,13 +12,26 @@ class ColorSchemes::CssVariables < Phlex::HTML
         #{css_variables}
       }
     CSS
+    if @my_color
+      unsafe_raw <<~CSS
+        :root {
+          #{my_css_variables}
+        }
+      CSS
+    end
   end
 
   def css_variables
-    color_scheme.weights.map { |weight, color| "--color-#{color_name}-#{weight}: #{to_hsla(color)};" }.join("\n\s\s")
+    weights.map { |weight, color| "--color-#{color_name}-#{weight}: #{to_hsla(color)};" }.join("\n\s\s")
+  end
+
+  def my_css_variables
+    weights.map { |weight, color| "--my-color-#{weight}: var(--color-#{color_name}-#{weight});" }.join("\n\s\s")
   end
 
   private
+
+  delegate :weights, to: :color_scheme
 
   def color_name = color_scheme.name.parameterize
 
