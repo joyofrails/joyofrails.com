7254ca154b6d73e5b9bebcb36a26e9fdaa5896e7
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-06 19:33:40 -0400

    Add alias methods for test helpers

--- spec/support/warden_helpers.rb
+++ spec/support/warden_helpers.rb
@@ -3,11 +3,13 @@ module WardenHelpers
     login_as(admin_user, scope: :admin_user)
     admin_user
   end
+  alias_method :login_as_admin, :login_admin_user
 
   def login_user(user = FactoryBot.create(:user))
     login_as(user, scope: :user)
     user
   end
+  alias_method :login_as_user, :login_user
 end
 
 RSpec.configure do |config|
