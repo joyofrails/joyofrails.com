f67c8f7dc52e3b2286a727d2a7700c0e300d9c1d
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-06 06:55:20 -0400

    Update ArticlePage.published logic

---
+++ spec/requests/site/articles_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe "Site: articles" do
+  let(:first_article) { ArticlePage.published.first }
+
+  # Filter out index.html.erb from article pages before selecting first draft
+  let(:first_draft) { ArticlePage.draft.lazy.filter { |article| article.page.request_path != "/articles" }.first }
+
+  describe "GET /articles" do
+    it "lists published articles" do
+      get "/articles"
+
+      expect(response).to have_http_status(:ok)
+
+      expect(response.body).to include(first_article.title)
+      expect(response.body).not_to include(first_draft.title)
+    end
+  end
+
+  describe "GET /drafts" do
+    it "lists draft articles" do
+      get "/drafts"
+
+      expect(response).to have_http_status(:ok)
+
+      expect(response.body).to include(first_draft.title)
+      expect(response.body).not_to include(first_article.title)
+    end
+  end
+end
