6c4809d06e6c072388ce510b980377f9a12c7386
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-05 23:08:33 -0400

    Remove method

--- app/views/components/application_component.rb
+++ app/views/components/application_component.rb
@@ -15,8 +15,4 @@ class ApplicationComponent < Phlex::HTML
   def markdown
     render Markdown::Application.new(yield)
   end
-
-  def flex_block(options = {}, &)
-    div(class: "flex items-start flex-col space-col-4 grid-cols-12 md:items-center md:flex-row md:space-row-4 #{options[:class]}", &)
-  end
 end
