95cbfeb6d7db5670cd94af7bb8b137054945e548
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-06 07:11:24 -0400

    Fix icon color and code title rendering

--- app/views/components/code_block/app_file.rb
+++ app/views/components/code_block/app_file.rb
@@ -12,7 +12,7 @@ class CodeBlock::AppFile < ApplicationComponent
   def view_template
     render ::CodeBlock::Article.new(**attributes) do |code_block|
       code_block.title do
-        link(app_file.repo_url, "Source code on Github", class: "nc flex items-center gap-1") {
+        link(app_file.repo_url, "Source code on Github", class: "nc") {
           plain app_file.app_path.to_s
           plain inline_svg_tag("external-link.svg", class: "icon icon-sm", height: 12, width: 12)
         }
