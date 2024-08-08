3e5e85d080743c1181a7f045d2a6f7295ae60127
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-08 07:03:48 -0400

    Add User to development seeds

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -2,9 +2,16 @@
 AdminUser.find_or_create_by!(
   email: "admin@example.com"
 ) do |u|
-  password = "password"
-  u.password = password
-  u.password_confirmation = password
+  u.password = "password"
+  u.password_confirmation = "password"
+end
+
+User.find_or_create_by(
+  email: "joy@example.com"
+) do |u|
+  u.password = "password"
+  u.password_confirmation = "password"
+  u.confirmed_at = Time.zone.now
 end
 
 ColorScheme.find_or_create_default
