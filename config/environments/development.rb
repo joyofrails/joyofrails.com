ec05fad758e0c4dbf8593949c525024df19a613b
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-09 21:55:36 -0400

    Add ability to test postmark email in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -89,4 +89,10 @@ Rails.application.configure do
   config.solid_cable.connects_to = {database: {writing: :cable, reading: :cable}}
 
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "debug")
+
+  config.settings.postmark_api_token = if ENV["ENABLE_POSTMARK_IN_DEV"] == "true"
+    Rails.application.credentials.postmark&.api_token || "POSTMARK_API_TEST"
+  else
+    "POSTMARK_API_TEST"
+  end
 end
