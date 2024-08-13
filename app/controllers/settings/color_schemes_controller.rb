afa5e47405c6ed179eb23e1ea35a1cbf7d213f5a
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-13 07:18:42 -0400

    Remove plausible event from color scheme controller
    Needs investigation: events triggered from backend have not been
    consistently showing up in plausible.

--- app/controllers/settings/color_schemes_controller.rb
+++ app/controllers/settings/color_schemes_controller.rb
@@ -1,6 +1,4 @@
 class Settings::ColorSchemesController < ApplicationController
-  after_action :send_plausible_event, only: :update
-
   def show
     @color_scheme = find_color_scheme
 
@@ -44,24 +42,5 @@ class Settings::ColorSchemesController < ApplicationController
     end
 
     redirect_to settings_color_scheme_path, status: :see_other
-  end
-
-  private
-
-  def send_plausible_event
-    plausible_event "Color Scheme Update", props: {color_scheme: @color_scheme.name}
-  end
-
-  def plausible_event(name, props: {})
-    Analytics::PlausibleEventJob.perform_later(
-      name: name,
-      url: request.original_url,
-      props: props,
-      referrer: request.referer,
-      headers: {
-        "User-Agent" => request.user_agent,
-        "X-Forwarded-For" => request.remote_ip
-      }
-    )
   end
 end
