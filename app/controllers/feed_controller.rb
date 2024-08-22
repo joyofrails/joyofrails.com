2a70ddc7cb4cef6a851d24a3506c54ffcb69f216
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-22 18:08:49 -0400

    Add conditional GET headers to feed response

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,9 +1,12 @@
 class FeedController < ApplicationController
   def index
     @articles = ArticlePage.published
-    respond_to do |format|
-      format.html { redirect_to feed_path(format: :atom), status: :moved_permanently }
-      format.atom
+
+    if stale?(@articles, public: true)
+      respond_to do |format|
+        format.html { redirect_to feed_path(format: :atom), status: :moved_permanently }
+        format.atom
+      end
     end
   end
 end
