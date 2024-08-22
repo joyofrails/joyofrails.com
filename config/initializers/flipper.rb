f32076224d92f6414de7e5445a7d6c77956cf2c5
Ross Kaffenberger <rosskaff@gmail.com>
2024-08-21 22:15:01 -0400

    Enable Flipper flags in development

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -3,3 +3,9 @@ require "flipper"
 Rails.application.configure do
   config.flipper.memoize = false
 end
+
+if Rails.env.development?
+  Flipper.enable(:user_registration)
+  Flipper.enable(:snippets)
+  Flipper.enable(:example_posts)
+end
