4902a022af2f3c4ab4f0712bfa18a9aca64dc729
Ross Kaffenberger <rosskaff@gmail.com>
2024-09-01 07:35:40 -0400

    Configure activestorage for digital ocean spaces

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,11 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = if Rails.root.join("tmp/storage-dev.txt").exist?
+    :digitalocean
+  else
+    :local
+  end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
