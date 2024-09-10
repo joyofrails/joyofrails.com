98503c322c66764dee671cc2bff191cda0858404
Ross Kaffenberger <rosskaff@gmail.com>
2024-09-10 18:04:01 -0400

    Introduce line wise syntax highlighter

--- app/views/components/code_block/code.rb
+++ app/views/components/code_block/code.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CodeBlock::Code < ApplicationComponent
   class << self
     def code_formatter
-      @code_formatter ||= Rouge::Formatters::HTML.new
+      @code_formatter ||= LineHighlighter.new(Rouge::Formatters::HTML.new, highlight_lines: 1..4)
     end
   end
 
@@ -30,4 +32,25 @@ class CodeBlock::Code < ApplicationComponent
   end
 
   def lexer = Rouge::Lexer.find(language) || Rouge::Lexers::PlainText
+
+  class LineHighlighter < Rouge::Formatter
+    def initialize(formatter, opts = {})
+      @formatter = formatter
+      @tag_name = opts.fetch(:tag_name, "div")
+      @class_format = opts.fetch(:class, "line-%i")
+      @highlight_line_class = opts.fetch(:highlight_line_class, "hll")
+      @highlight_lines = opts[:highlight_lines] || []
+    end
+
+    def stream(tokens, &b)
+      token_lines(tokens).with_index(1) do |line_tokens, lineno|
+        classes = sprintf(@class_format, lineno)
+        classes += " #{@highlight_line_class}" if @highlight_lines.include? lineno
+
+        yield %(<#{@tag_name} class="#{classes}">)
+        @formatter.stream(line_tokens) { |formatted| yield formatted }
+        yield %(\n</#{@tag_name}>)
+      end
+    end
+  end
 end
