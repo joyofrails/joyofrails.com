957c05a35814f347a85fe35aee6aac4a448da783
Ross Kaffenberger <rosskaff@gmail.com>
2024-09-13 07:58:21 -0400

    Run rails app:update for Rails 7.2

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,8 @@
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :screenshot
+  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn,
+
+  # Custom parameters
+  :screenshot
 ]
