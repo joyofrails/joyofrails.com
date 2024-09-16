69bc4354fa1e7e20f3c80853fd23ea1610f118a2
Ross Kaffenberger <rosskaff@gmail.com>
2024-09-16 07:06:55 -0400

    Remove unused manifest

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,7 @@ module ApplicationHelper
     set_meta_tags icon: [
       {rel: "icon", href: "/favicon.ico?v1.0", sizes: "32x32"},
       {rel: "icon", href: "/icon.svg?v1.0", type: "image/svg+xml"},
-      {rel: "apple-touch-icon", href: "/apple-touch-icon.png?v1.0"},
-      {rel: "manifest", href: "/manifest.webmanifest?v1.0"}
+      {rel: "apple-touch-icon", href: "/apple-touch-icon.png?v1.0"}
     ]
     set_meta_tags index: true
     set_meta_tags icon: "/favicon.ico"
