2647075fe0bf6285266ff46a55c3c388c5a53c6a
Ross Kaffenberger <rosskaff@gmail.com>
2024-10-14 09:49:51 -0400

    Add spec for article

---
+++ spec/system/articles/mastering-custom-configuration-in-rails_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "Mastering Custom Configuration in Rails", type: :system do
+  it "displays the article content" do
+    visit "/articles/mastering-custom-configuration-in-rails"
+
+    expect(page).to have_content "Mastering Custom Configuration in Rails"
+  end
+end
