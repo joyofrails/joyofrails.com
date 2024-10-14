c513ce5737d1b4b8812aeda81e06a4f45b915c58
Ross Kaffenberger <rosskaff@gmail.com>
2024-10-14 08:56:16 -0400

    Fix spelling in comment

--- config/initializers/vapid.rb
+++ config/initializers/vapid.rb
@@ -3,14 +3,14 @@ Rails.application.configure do
   # To regenerate the VAPID keys, run the following command:
   # bin/rails runner "puts WebPush.generate_key.to_h.to_yaml"
   #
-  # The Ruby WebPush library can produce a padded public key unncessarily.
+  # The Ruby WebPush library can produce a padded public key unnecessarily.
   # We delete the trailing "=" from the public key so we can subscribe in JavaScript
   # (i.e. registration.pushManager.subscribe({ applicationServerKey: webPushKey }))
   # with the raw key; otherwise we have to convert to a Uint8Array as most docs suggest
   # which is not necessary for the WebPush API and just doesn't feel like a nice API.
   #
-  config.x.vapid.public_key = Rails.application.credentials.dig(:vapid, :public_key).to_s.delete("=") # The VAPID public key
-  config.x.vapid.private_key = Rails.application.credentials.dig(:vapid, :private_key).to_s.delete("=") # The VAPID private key
+  config.x.vapid.public_key = credentials.dig(:vapid, :public_key).to_s.delete("=") # The VAPID public key
+  config.x.vapid.private_key = credentials.dig(:vapid, :private_key).to_s.delete("=") # The VAPID private key
 
-  config.x.vapid.subject = Rails.application.credentials.dig(:vapid, :subject) # The "mailto:" email address
+  config.x.vapid.subject = credentials.dig(:vapid, :subject) # The "mailto:" email address
 end
