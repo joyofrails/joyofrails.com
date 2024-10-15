af3465909c11fd344e6ac25197d8f8e73eca8de1
Ross Kaffenberger <rosskaff@gmail.com>
2024-10-15 07:26:02 -0400

    Extract ColorScheme.bulk_load from rake task

--- spec/models/color_scheme_spec.rb
+++ spec/models/color_scheme_spec.rb
@@ -13,6 +13,93 @@ RSpec.describe ColorScheme, type: :model do
     end
   end
 
+  describe "self.bulk_load" do
+    it "should create new color schemes from hash object" do
+      data = {
+        Heliotrope: {
+          "50": "#fbf5ff",
+          "100": "#f5e8ff",
+          "200": "#edd6fe",
+          "300": "#dfb5fd",
+          "400": "#cf90fa",
+          "500": "#b758f4",
+          "600": "#a437e6",
+          "700": "#8e25cb",
+          "800": "#7724a5",
+          "900": "#621e85",
+          "950": "#430962"
+        },
+
+        Watercourse: {
+          "50": "#ebfef6",
+          "100": "#cffce7",
+          "200": "#a3f7d4",
+          "300": "#68edbf",
+          "400": "#2ddaa4",
+          "500": "#08c18e",
+          "600": "#009d74",
+          "700": "#007d60",
+          "800": "#026b53",
+          "900": "#035141",
+          "950": "#002e25"
+        }
+      }
+
+      expect {
+        ColorScheme.bulk_load(data)
+      }.to change(ColorScheme, :count).by(2)
+
+      expect(ColorScheme.find_by(name: "Heliotrope")).to be_present
+      expect(ColorScheme.find_by(name: "Watercourse")).to be_present
+    end
+
+    it "should allow customization of name" do
+      data = {
+        heliotrope: {
+          "50": "#fbf5ff",
+          "100": "#f5e8ff",
+          "200": "#edd6fe",
+          "300": "#dfb5fd",
+          "400": "#cf90fa",
+          "500": "#b758f4",
+          "600": "#a437e6",
+          "700": "#8e25cb",
+          "800": "#7724a5",
+          "900": "#621e85",
+          "950": "#430962"
+        }
+      }
+
+      ColorScheme.bulk_load(data) { |name| "Custom #{name.titleize}" }
+
+      expect(ColorScheme.find_by(name: "Custom Heliotrope")).to be_present
+    end
+
+    it "should not duplicate" do
+      FactoryBot.create(:color_scheme, name: "Custom Heliotrope")
+
+      data = {
+        heliotrope: {
+          "50": "#fbf5ff",
+          "100": "#f5e8ff",
+          "200": "#edd6fe",
+          "300": "#dfb5fd",
+          "400": "#cf90fa",
+          "500": "#b758f4",
+          "600": "#a437e6",
+          "700": "#8e25cb",
+          "800": "#7724a5",
+          "900": "#621e85",
+          "950": "#430962"
+        }
+      }
+
+      expect {
+        ColorScheme.bulk_load(data) { |name| "Custom #{name.titleize}" }
+      }.not_to change(ColorScheme, :count)
+    end
+  end
+
   describe "#weights" do
     it "should return a hash of all weights" do
       color_scheme = FactoryBot.build(:color_scheme)
