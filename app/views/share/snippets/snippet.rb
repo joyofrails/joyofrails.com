f78ccb1170edf26133b46ebd22ece761b0799a99
Ross Kaffenberger <rosskaff@gmail.com>
2024-10-16 09:23:10 -0400

    Extract Snippet component

---
+++ app/views/share/snippets/snippet.rb
@@ -0,0 +1,40 @@
+module Share
+  module Snippets
+    class Snippet < ApplicationComponent
+      include Phlex::Rails::Helpers::DOMID
+
+      attr_accessor :current_user, :snippet, :attributes
+
+      def initialize(snippet, current_user:, **attributes)
+        @snippet = snippet
+        @current_user = current_user
+        @attributes = attributes
+      end
+
+      def view_template
+        div(**mix(attributes, class: "section-content", id: dom_id(snippet))) do
+          if snippet.title
+            h2 { snippet.title }
+          end
+
+          if snippet.description
+            render Markdown::Safe.new(snippet.description)
+          end
+
+          render CodeBlock::Snippet.new(snippet)
+
+          div do
+            render Pages::Timestamp.new \
+              published_on: snippet.created_at.to_date,
+              updated_on: snippet.updated_at.to_date,
+              class: "text-small"
+          end
+
+          div do
+            render Share::Snippets::Toolbar.new(snippet, current_user: current_user)
+          end
+        end
+      end
+    end
+  end
+end
