6d62b730262f8b271308bd9db9241e41cc44433d
Ross Kaffenberger <rosskaff@gmail.com>
2024-10-16 09:39:20 -0400

    Use Snippet component on show page

--- app/views/share/snippets/snippet.rb
+++ app/views/share/snippets/snippet.rb
@@ -3,36 +3,49 @@ module Share
     class Snippet < ApplicationComponent
       include Phlex::Rails::Helpers::DOMID
 
-      attr_accessor :current_user, :snippet, :attributes
+      attr_accessor :current_user, :snippet, :href, :attributes
 
-      def initialize(snippet, current_user:, **attributes)
+      def initialize(snippet, current_user:, href: false, **attributes)
         @snippet = snippet
+        @href = href
         @current_user = current_user
         @attributes = attributes
       end
 
       def view_template
         div(**mix(attributes, class: "section-content", id: dom_id(snippet))) do
-          if snippet.title
-            h2 { snippet.title }
-          end
-
-          if snippet.description
-            render Markdown::Safe.new(snippet.description)
-          end
-
-          render CodeBlock::Snippet.new(snippet)
-
-          div do
-            render Pages::Timestamp.new \
-              published_on: snippet.created_at.to_date,
-              updated_on: snippet.updated_at.to_date,
-              class: "text-small"
+          if href
+            a(href: href) do
+              div(class: "section-content overflow-x-auto keep-style-when-linked") do
+                content
+              end
+            end
+          else
+            content
           end
 
           div do
             render Share::Snippets::Toolbar.new(snippet, current_user: current_user)
           end
+        end
+      end
+
+      def content
+        if snippet.title
+          h2 { snippet.title }
+        end
+
+        if snippet.description
+          render Markdown::Safe.new(snippet.description)
+        end
+
+        render CodeBlock::Snippet.new(snippet)
+
+        div(class: "keep-style-when-linked") do
+          render Pages::Timestamp.new \
+            published_on: snippet.created_at.to_date,
+            updated_on: snippet.updated_at.to_date,
+            class: "text-small"
         end
       end
     end
