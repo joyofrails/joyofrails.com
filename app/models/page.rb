df8f2e663e9607a91ad8eee0f7167bdbd2699f49
Ross Kaffenberger <rosskaff@gmail.com>
2024-10-27 00:32:03 -0400

    Implement search index syncing for Page model
    Taking inspiration from blog posts:
    
    * https://za.chari.as/2024/09/09/sqlite-full-text-search.html
    * https://mariochavez.io/desarrollo/2023/09/01/full-text-search-with-sqlite-and-rails/

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,54 @@
 # ActiveRecord model to represent a static page in the database.
 class Page < ApplicationRecord
+  after_create_commit :create_in_search_index
+  after_update_commit :update_in_search_index
+  after_destroy_commit :remove_from_search_index
+
+  def self.rebuild_search_index
+    find_each(&:update_in_search_index)
+  end
+
+  def title
+    resource.data.title
+  end
+
+  def body
+    resource.body
+  end
+
+  def body_html
+    ApplicationController.render(inline: body, type: resource.handler, layout: false)
+  end
+
+  def body_text
+    Sanitize.clean(body_html).squish
+  end
+
   def resource
     Sitepress.site.get(request_path)
+  end
+
+  def create_in_search_index
+    Rails.logger.info "[#{self.class}] Creating search index for page #{id} #{request_path}"
+    execute_sanitized_sql "INSERT INTO pages_search_index (page_id, title, body) VALUES (?, ?, ?)", id, title, body_text
+  end
+
+  def update_in_search_index
+    transaction do
+      remove_from_search_index
+      create_in_search_index
+    end
+  end
+
+  def remove_from_search_index
+    execute_sanitized_sql "DELETE FROM pages_search_index WHERE page_id = ?", id
+  end
+
+  def execute_sanitized_sql(*statement)
+    execute self.class.sanitize_sql(statement)
+  end
+
+  def execute(statement)
+    self.class.connection.execute statement
   end
 end
