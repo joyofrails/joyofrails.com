6fd9cc7750a3b74ef7f371f102005f8e65f4f4fa
Ross Kaffenberger <rosskaff@gmail.com>
2024-10-26 23:49:25 -0400

    Update migration

--- db/migrate/20241027033302_create_pages.rb
+++ db/migrate/20241027033302_create_pages.rb
@@ -1,6 +1,7 @@
 class CreatePages < ActiveRecord::Migration[8.0]
   def change
-    create_table :pages do |t|
+    create_table :pages, force: true, id: false do |t|
+      t.primary_key :id, :string, default: -> { "ULID()" }
       t.string :request_path, null: false, index: {unique: true}
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -165,7 +165,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_10_27_033302) do
     t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
   end
 
-  create_table "pages", force: :cascade do |t|
+  create_table "pages", id: :string, default: -> { "ULID()" }, force: :cascade do |t|
     t.string "request_path", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
