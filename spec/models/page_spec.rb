2ac9bcaaf6d999f3b833dcf72284329b5b85eb7c
Ross Kaffenberger <rosskaff@gmail.com>
2024-10-27 23:04:15 -0400

    Update page model search for fuzzy match and remove special characters
    The search query will raise SQLite errors when it doesn’t like the
    special character combinations, like unclosed quotes.

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -6,4 +6,34 @@ RSpec.describe Page, type: :model do
 
     expect(page.resource).to eq Sitepress.site.get("/")
   end
+
+  describe ".rebuild_search_index" do
+    it "doesn’t blow up" do
+      Page.find_or_create_by!(request_path: "/")
+
+      Page.rebuild_search_index
+    end
+  end
+
+  describe ".search" do
+    it "allows searching for pages" do
+      page = Page.find_or_create_by!(request_path: "/")
+
+      expect(Page.search("Joy of Rails")).to include page
+    end
+
+    it "ignores special characters in the query" do
+      page = Page.find_or_create_by!(request_path: "/")
+
+      expect(Page.search("Joy\"&^ of Rails")).to include page
+    end
+
+    it "works after rebuilding index" do
+      page = Page.find_or_create_by!(request_path: "/")
+
+      Page.rebuild_search_index
+
+      expect(Page.search("Joy of Rails")).to include page
+    end
+  end
 end
