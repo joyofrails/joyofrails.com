8b7377f3cd7ef7c0e80ad5f5f6f0267fa53ab7ff
Ross Kaffenberger <rosskaff@gmail.com>
2024-10-31 07:53:23 -0400

    Add comments

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,7 +6,19 @@ class Page < ApplicationRecord
   after_update_commit :update_in_search_index
   after_destroy_commit :remove_from_search_index
 
+  # The pages_search_index table is a full-text search index for the pages
+  # table. FTS5 tables contain a hidden 'rank' column that is (hand waving) a
+  # relevancy score for sorting search results.
+  # https://www.sqlite.org/fts5.html#sorting_by_auxiliary_function_results
+  #
   scope :ranked, -> { order(:rank) }
+
+  # snippet() is a SQLite function that returns a snippet of text containing the search term.
+  # It’s used to highlight search results. The snippet() function is not
+  # available in all databases, so this scope is only available for SQLite as
+  # part of the FTS5 extension.
+  # https://www.sqlite.org/fts5.html#the_snippet_function
+  #
   scope :with_snippets, ->(**options) do
     select("#{table_name}.*")
       .select("snippet(pages_search_index, 0, '<mark>', '</mark>', '…', 32) AS title_snippet")
