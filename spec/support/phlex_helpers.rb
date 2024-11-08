122937e8f8886e212cfaabcecb5a553b67070097
Ross Kaffenberger <rosskaff@gmail.com>
2024-11-08 06:03:21 -0500

    Extract simple view spec helper for phlex components

--- spec/support/phlex_helpers.rb
+++ spec/support/phlex_helpers.rb
@@ -1,4 +1,12 @@
+module PhlexHelpers
+  def render_component(*, **, &)
+    render(described_class.new(*, **, &))
+  end
+end
+
 RSpec.configure do |config|
+  config.include PhlexHelpers, type: :view
+
   config.before(:each, type: :view) do
     allow(view).to receive(:headers).and_return({"Content-Type" => "text/html"})
   end
