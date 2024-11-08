24ae95b546a7b9251cc053cac779f4a0a8026cb1
Ross Kaffenberger <rosskaff@gmail.com>
2024-11-08 14:07:19 -0500

    Apply Rubocop settings to erb_lint and make corrections

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,4 +1,11 @@
 class Admin::HomeController < Admin::BaseController
   def index
+    @nav = {
+      "Rails Admin" => "/admin/data",
+      "Newsletters" => admin_newsletters_path,
+      "Flipper" => "/admin/flipper",
+      "Mission Control Jobs" => "/admin/jobs",
+      "Litestream" => "/admin/litestream"
+    }
   end
 end
