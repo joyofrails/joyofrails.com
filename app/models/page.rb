5f0f2cda9f916b7574574365dea55a4329ae093f
Ross Kaffenberger <rosskaff@gmail.com>
2024-11-08 22:07:25 -0500

    Extract method for Page text rendering to SitepressPage

--- app/models/page.rb
+++ app/models/page.rb
@@ -17,33 +17,15 @@
 class Page < ApplicationRecord
   include Page::Searchable
 
-  def title
-    resource.data.title
-  end
+  def resource = Sitepress.site.get(request_path)
 
-  def body
-    resource.body
-  end
-
-  def body_html
-    ApplicationController.render(
-      inline: body,
-      type: (resource.handler.to_sym == :mdrb) ? :"mdrb-atom" : resource.handler,
-      layout: false,
-      content_type: "application/atom+xml",
-      assigns: {
-        format: :atom
-      }
-    )
-  end
-
-  def body_text
-    Nokogiri::HTML(body_html).text.squish
-  end
-
-  def resource
-    Sitepress.site.get(request_path)
-  end
+  def body_text = Nokogiri::HTML(SitepressPage.render_html(resource)).text.squish
 
   def url = request_path
+
+  def title = resource.data.title
+
+  def body = resource.body
+
+  def description = resource.data.description
 end
