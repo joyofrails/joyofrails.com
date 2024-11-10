0d975edd4bf98b1be00b04bdcf6124206bc4bd26
Ross Kaffenberger <rosskaff@gmail.com>
2024-11-09 19:44:51 -0500

    Save analyze topics VCR cassette
    We’re not actually using this in the specs but may help serve as a good
    reference for debugging the request

--- spec/support/webmock_vcr.rb
+++ spec/support/webmock_vcr.rb
@@ -16,6 +16,8 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.ignore_localhost = true
   config.configure_rspec_metadata!
+
+  config.filter_sensitive_data("<AUTHORIZATION>") { |interaction| Array(interaction.request.headers["Authorization"]).first }
 end
 
 RSpec.configure do |config|
