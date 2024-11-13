4a1b236ccd28e0697949292fd9ca5047a940eea3
Ross Kaffenberger <rosskaff@gmail.com>
2024-11-13 07:17:51 -0500

    First pass at Turbo topic navigation with sidebar

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,10 +1,10 @@
 class TopicsController < ApplicationController
   def index
     @topics = Topic.approved.with_pages.order(name: :asc)
-    @topics = @topics.where("lower(slug) LIKE ?", "#{params[:letter].downcase}%") if params[:letter].present?
   end
 
   def show
+    @topics = Topic.approved.with_pages.order(name: :asc)
     @topic = Topic.find_by!(slug: params[:slug])
   end
 end
