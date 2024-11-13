d8aab0e50bb067c2c698b6fb9bbdcc703f36ac7d
Ross Kaffenberger <rosskaff@gmail.com>
2024-11-13 18:59:14 -0500

    Make flex_block helper more flexible for options

--- app/views/components/phlex_concerns/flex_block.rb
+++ app/views/components/phlex_concerns/flex_block.rb
@@ -1,5 +1,8 @@
 module PhlexConcerns::FlexBlock
   def flex_block(options = {}, &)
-    div(class: "flex items-start flex-col space-col-4 grid-cols-12 md:items-center md:flex-row md:space-row-4 #{options[:class]}", &)
+    div(
+      **mix(options, class: "flex items-start flex-col space-col-4 grid-cols-12 md:items-center md:flex-row md:space-row-4"),
+      &
+    )
   end
 end
