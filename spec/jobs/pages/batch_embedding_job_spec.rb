db4182bac274b40cc67ddd6b95803045e38967e8
Ross Kaffenberger <rosskaff@gmail.com>
2024-11-16 06:11:34 -0600

    Update spec to improve coverage

--- spec/jobs/pages/batch_embedding_job_spec.rb
+++ spec/jobs/pages/batch_embedding_job_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Pages::BatchEmbeddingJob, type: :job do
   it "doesn’t blow up" do
-    Page.upsert_collection_from_sitepress!(limit: 3)
+    FactoryBot.create(:page, :published)
 
     described_class.perform_now
   end
