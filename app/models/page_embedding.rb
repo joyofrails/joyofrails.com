d89cf4dcdd925433c7cd0fd950d3e44425369e6f
Ross Kaffenberger <rosskaff@gmail.com>
2024-11-19 10:00:32 -0500

    Refactor Vector serialization to work better with AR
    sqlite-vec will convert a JSON.encoded array for us on insert. We want
    to avoid binary-unpack on deserializing this instance because the
    Ruby-representation will still be the JSON encoded array. After the
    record is loaded from the database however, the "before type cast" value
    will be the binary representation, at which point, we can use
    String#unpack. Accounting for this allows us to replace the
    "custom_create" method.

--- app/models/page_embedding.rb
+++ app/models/page_embedding.rb
@@ -14,13 +14,6 @@ class PageEmbedding < ApplicationRecord
       .order(distance: :asc)
   }
 
-  def self.custom_create(id:, embedding:)
-    connection.execute sanitize_sql([<<~SQL.squish, id, embedding.to_s])
-      INSERT INTO page_embeddings (id, embedding)
-      VALUES (?, ?)
-    SQL
-  end
-
   # SQLite supports upserts via INSERT ON CONFLICT DO UPDATE for normal tables
   # but not for virtual tables. This method behaves like an upsert the embedding
   # for a page by first removing the existing embedding if it exists and then
@@ -28,7 +21,7 @@ class PageEmbedding < ApplicationRecord
   def self.upsert_embedding!(page, embedding)
     transaction do
       page.page_embedding&.destroy
-      custom_create(id: page.id, embedding: embedding)
+      create(id: page.id, embedding: embedding)
     end
   end
 
--- app/models/types/vector.rb
+++ app/models/types/vector.rb
@@ -1,7 +1,9 @@
 # Custom type for ActiveRecord to deserialize vectors from vector columns
 # enabled by sqlite-vec extensions which provides vec0 virtual tables.
 module Types
-  class Vector < ActiveRecord::Type::Json
+  class Vector < ActiveModel::Type::Value
+    include ActiveModel::Type::SerializeCastValue
+
     def type
       :vector
     end
@@ -9,11 +11,25 @@ module Types
     def deserialize(value)
       return value unless value.is_a?(::String)
 
-      begin
-        value.unpack("F*")
-      rescue
-        nil
+      # When we initially created the vector column, we serialized the vector as JSON
+      if value.start_with?("[")
+        ActiveSupport::JSON.decode(value)
+      else
+        # Unpacks a float array from the binary string as represented in the
+        # database. The format is "f*" per sqlite-vec docs. The "f*" template is
+        # a "single-precision float directive" per Ruby docs
+        # https://docs.ruby-lang.org/en/3.3/packed_data_rdoc.html
+        #
+        # There are minor precision issues when unpacking the binary string so
+        # we prefer to use subqueries for embedding MATCH comparision over
+        # deserializing/serialize in ActiveRecord.
+        #
+        value.unpack("f*")
       end
+    end
+
+    def serialize(value)
+      ActiveSupport::JSON.encode(value) unless value.nil?
     end
   end
 end
