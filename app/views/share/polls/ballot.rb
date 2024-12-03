e63f002e2edce11b7e4ea3724caebb7567a569bb
Ross Kaffenberger <rosskaff@gmail.com>
2024-12-03 09:58:49 -0500

    Extract Phlex component for poll ballot

---
+++ app/views/share/polls/ballot.rb
@@ -0,0 +1,35 @@
+module Share
+  module Polls
+    class Ballot < ApplicationComponent
+      include Phlex::Rails::Helpers::ButtonTo
+      include Phlex::Rails::Helpers::Routes
+      include Phlex::Rails::Helpers::Tag
+      include PhlexConcerns::NestedDomId
+
+      attr_accessor :poll
+
+      def initialize(poll)
+        @poll = poll
+      end
+
+      def view_template
+        div id: nested_dom_id(poll, "ballot") do
+          poll.questions.each do |question|
+            div id: nested_dom_id(poll, question),
+              class: "question flex flex-col gap-2" do
+              p { question.body }
+
+              question.answers.each do |answer|
+                div id: nested_dom_id(question, answer) do
+                  button_to answer.body,
+                    share_poll_votes_path(poll, answer_id: answer.id),
+                    class: "button transparent slim"
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
