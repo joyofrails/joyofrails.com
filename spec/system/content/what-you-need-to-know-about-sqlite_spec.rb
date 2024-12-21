6a421ed2d5d637870c1863b33766357d3a634e54
Ross Kaffenberger <rosskaff@gmail.com>
2024-12-20 21:42:18 -0500

    Re-enable system test

--- spec/system/content/what-you-need-to-know-about-sqlite_spec.rb
+++ spec/system/content/what-you-need-to-know-about-sqlite_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe "What you need to know about SQLite", type: :system do
     expect(document).to have_content "What you need to know about SQLite"
   end
 
-  xit "displays the article content with poll by primary author" do
+  it "displays the article content with poll by primary author" do
     FactoryBot.create(:user, :primary_author)
 
     visit "/articles/what-you-need-to-know-about-sqlite"
 
     expect(document).to have_content "What you need to know about SQLite"
 
-    expect(document).to have_content "How likely are you to use SQLite in your Rails app?"
+    expect(document).to have_content "How likely are you to use SQLite in your next Rails app?"
   end
 end
