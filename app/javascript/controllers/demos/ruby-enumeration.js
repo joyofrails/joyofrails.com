f42aba1603a47a7e08cf299dc70f556f9a778b36
Ross Kaffenberger <rosskaff@gmail.com>
2024-12-23 11:20:39 -0500

    Just render the demo straight to iframe
    With the JS controller I added previously to update the frame src,
    there’s no real need to go to the server for the color scheme since we
    can query CSS variables from JS.

--- app/javascript/controllers/demos/ruby-enumeration.js
+++ app/javascript/controllers/demos/ruby-enumeration.js
@@ -5,6 +5,9 @@ import { isDarkMode } from '../darkmode';
 
 const console = debug('app:javascript:controllers:demos:ruby-enumeration');
 
+const cssThemeVariable = (prop) =>
+  window.getComputedStyle(document.documentElement).getPropertyValue(prop);
+
 export default class extends Controller {
   static values = {
     url: String,
@@ -13,15 +16,27 @@ export default class extends Controller {
   connect() {
     console.log('connect');
 
-    const turboFrame = this.element;
-    const url = new URL(this.urlValue);
+    const iFrame = this.element;
+    const url = new URL(this.urlValue || iFrame.src);
 
-    // Determine if the page is in Dark Mode
-    url.searchParams.append('is_dark_mode', isDarkMode());
+    if (isDarkMode()) {
+      url.searchParams.append('isDarkMode', 'true');
+    } else {
+      url.searchParams.append(
+        'background',
+        cssThemeVariable('--joy-background'),
+      );
+    }
+
+    url.searchParams.append(
+      'gridLines',
+      cssThemeVariable('--joy-border-subtle'),
+    );
+
+    url.searchParams.append('animationSpeed', 0.3);
 
     console.log('url', url.toString());
 
-    // Set the src attribute of the Turbo Frame element
-    turboFrame.src = url.toString();
+    iFrame.src = url.toString();
   }
 }
