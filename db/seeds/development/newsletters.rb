e25255acdd99a534fd48499cf6a0882ab87debf1
Ross Kaffenberger <rosskaff@gmail.com>
2024-12-31 09:55:17 -0500

    Run bundle update brakeman --conservative

--- db/seeds/development/newsletters.rb
+++ db/seeds/development/newsletters.rb
@@ -1,6 +1,6 @@
 start_count_newsletters = 5
 newsletter_fill_count = start_count_newsletters - Newsletter.count
-require 'factory_bot'
-require 'faker'
-require_relative '../../../spec/factories/newsletters'
+require "factory_bot"
+require "faker"
+require_relative "../../../spec/factories/newsletters"
 FactoryBot.create_list(:newsletter, newsletter_fill_count) if newsletter_fill_count > 0
