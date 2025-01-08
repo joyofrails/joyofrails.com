8f11d66acc34137b9ce5d6de9342d6d417fd7f3d
Ross Kaffenberger <rosskaff@gmail.com>
2025-01-07 23:22:29 -0500

    Add system spec

--- spec/system/content/simple-trick-to-understand-ruby-lazy-enumerator_spec.rb
+++ spec/system/content/simple-trick-to-understand-ruby-lazy-enumerator_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe "A simple trick to understand Ruby’s lazy enumerator", type: :s
 
     expect(document).to have_content "A simple trick to understand Ruby’s lazy enumerator"
   end
+
+  it "displays the article content with poll by primary author" do
+    FactoryBot.create(:user, :primary_author)
+
+    visit "/articles/simple-trick-to-understand-ruby-lazy-enumerator"
+
+    expect(document).to have_content "Lazy Enumerator Live Poll"
+
+    expect(document).to have_content "How often do you reach for Ruby’s lazy enumerator?"
+  end
 end
